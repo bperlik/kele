dd3163d8fec3ce1485c586b2e6bd381003fccf6f
Barbara Perlik <barbperlik@gmail.com>
2017-07-03 14:01:49 -0400

    retrieve hash for user body

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -9,8 +10,12 @@ class Kele
         "email": email,
         "password": password
      })
-    puts response.code
     raise "Invalid Email or Password. Try Again." if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 end
