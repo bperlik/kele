5e6f637b4309a7f3a12f9c1480a2589556177530
Barbara Perlik <barbperlik@gmail.com>
2017-07-06 16:34:35 -0400

    get and create messages

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,6 +14,7 @@ class Kele
      })
     raise "Invalid Email or Password. Try Again." if response.code == 404
     @auth_token = response["auth_token"]
+    puts @auth_token
   end
 
   def get_me
@@ -24,5 +25,28 @@ class Kele
   def get_mentor_availability(mentor_id)
     response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_messages(page_number=nil)
+    if page_number == nil
+      response = self.class.get("/message_threads", headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get("/message_threads?page=#{page_number}", headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped_text)
+    response = self.class.post("/messages",            # sender, recipient_id, and stripped text are required
+      body: {
+       "sender": sender,                               # my email
+       "recipient_id": recipient_id,                   # Rizwan id 2292457
+       "token": token,                                 # message thread token or nil for new thread
+       "subject": subject,                             # string
+       "stripped_text": stripped_text                  # string
+     },
+     headers: { "authorization" => @auth_token, content_type: "application/json" })
+    p response
+    puts response.body
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,10 +1,12 @@
 module Roadmap
   def get_roadmap(roadmap_id)
+    # use roadmap_id 31 for testing
     response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
     @roadmap = JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
+    # use checkpoint_id 2467 for testing
     response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
     @checkpoint = JSON.parse(response.body)
   end
